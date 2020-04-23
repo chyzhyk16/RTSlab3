80d633769193a40ae2e768f78e81858bfde3efba
Vlad Chyzhevych <chyzhk16@gmail.com>
2020-04-23 10:34:01 +0300

    lab3_2 file

---
+++ lab3_2.py
@@ -0,0 +1,28 @@
+import time
+
+
+def calc2(learning_rate, maxtime, maxiter):
+    iter = 0
+    p = 4
+    points = [[0, 6], [1, 5], [3, 3], [2, 4]]
+    w1 = 0
+    w2 = 0
+    flag = False
+    start = time.time()
+    while iter < maxiter and maxtime > time.time() - start:
+        y = points[iter % 4][0] * w1 + points[iter % 4][1] * w2
+        delta = p - y
+        w1 += delta * points[iter % 4][0] * learning_rate
+        w2 += delta * points[iter % 4][1] * learning_rate
+        iter += 1
+        if points[0][0] * w1 + points[0][1] * w2 > p > points[2][0] * w1 + points[2][1] * w2 and points[1][0] * w1 + \
+                points[1][1] * w2 > p > points[3][0] * w1 + points[3][1] * w2:
+            flag = True
+            return 'w1=' + str(w1)[:5] + ' w2=' + str(w2)[:5] + '\ntime=' + str(
+                time.time() - start)
+
+            break
+    if flag:
+        return None
+    else:
+        return 'w1,w2 не розроховано до дедлайну'
